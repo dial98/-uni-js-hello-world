45498994f2532579282d5c6e699d1f09925d06dc
dial98 <dial98>
2019-01-28 21:32:59 +0200

    Commit One.

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Hello world!");
+
+function functionName(){
+    return "Hello JS!";
+}
+console.log(functionName());
+
+var firstWord="Hello";
+var secondWord="I'm Diana";
+var thirdWord="From Plovdiv University!";
+var senetence=firstWord+", "+secondWord+" "+thirdWord;
+console.log(senetence);
